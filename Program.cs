1120f1ab1424722f1f69699e998b7616df2c4fb8
Trần Trọng Nghĩa <nghiatrantrong188@gmail.com>
2023-03-31 13:30:52 +0700

    UsingOperator

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace UsingOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+            float area;
+            area = width * height;
+            Console.WriteLine("Area is: " + area);
+            area = float.Parse(Console.ReadLine());
+        }
+    }
+}
